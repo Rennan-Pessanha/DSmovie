0af2dbacf150504051e79fece997762260781c56
Rennan Pessanha <94031223+Rennan-Pessanha@users.noreply.github.com>
2022-05-12 01:38:36 -0300

    Routes

---
+++ frontend/src/pages/form/index.tsx
@@ -0,0 +1,8 @@
+function form() {
+    return (
+        <h1>pagina de form</h1>
+    )
+
+}
+
+export default form
---
+++ frontend/src/pages/listing/index.tsx
@@ -0,0 +1,8 @@
+function listing() {
+    return (
+        <h1>title</h1>
+    )
+
+}
+
+export default listing
