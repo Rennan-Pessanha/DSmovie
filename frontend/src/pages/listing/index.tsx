29d2a34bc5a59bdc3b0c6d47edf96ab035806a50
Rennan Pessanha <94031223+Rennan-Pessanha@users.noreply.github.com>
2022-05-15 00:38:20 -0300

    Finalização do Front-End

--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -1,6 +1,36 @@
+import MovieCard from "components/MovieCard"
+import Pagination from "components/Pagination"
+
+
 function listing() {
     return (
-        <h1>title</h1>
+        <>
+            <Pagination />
+
+
+        <div className="container">
+        <div className='row'>
+            <div className="col-sm-6">
+            <MovieCard />
+            </div>
+            <div className="col-sm-6">
+            <MovieCard />
+            </div>
+            <div className="col-sm-6">
+            <MovieCard />
+            </div>
+            <div className="col-sm-6">
+            <MovieCard />
+            </div>
+            <div className="col-sm-6">
+            <MovieCard />
+            </div>
+        </div>
+        </div>
+
+
+
+        </>
     )
 
 }
